fe13ff632ebd1d8e88ec6bca9239fea1f8e6afb8
Sariqua <104137948+Sariqua@users.noreply.github.com>
2022-12-10 19:03:13 +0530

    react

---
+++ src/Components/Expenseitems.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+   return(
+<div>
+    <div>Food Rs 10 </div>
+    <div>
+        <h2> Petrol 100</h2>
+        <div>
+            Movie Rs 200
+        </div>
+    </div>
+</div>
+   );
+}
+
+export default ExpenseItem;
